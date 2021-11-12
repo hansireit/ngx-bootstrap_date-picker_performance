32c7790e80fd79dd41a460d42ec0e26d0ab28d19
Johannes Reitbauer <johannes.reitbauer@jungheinrich.at>
2021-11-12 14:29:25 +0100

    Fixed config, automatic clicking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,50 @@
-import { Component, VERSION } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  QueryList,
+  ViewChildren,
+} from "@angular/core";
+import { Subscription } from "rxjs";
+import { DatePickerComponent } from "./date-picker/date-picker.component";
 
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  readonly numberOfInputs = 20;
+export class AppComponent implements AfterViewInit {
+  readonly numberOfInputs = 10;
   readonly inputs: string[] = [];
+
+  @ViewChildren(DatePickerComponent)
+  datePicker: QueryList<DatePickerComponent>;
+
+  clickCount = 0;
+  intervalSubscription: number;
 
   constructor() {
     for (let i = 0; i < this.numberOfInputs; i++) {
       this.inputs.push(`Input ${i}`);
     }
+  }
+  ngAfterViewInit(): void {
+    console.log(this.datePicker);
+    this.startClicking();
+  }
+
+  startClicking(): void {
+    this.clickCount = 0;
+
+    setInterval(() => {
+      const index = this.getRandomIntInclusive(0, this.datePicker.length - 1);
+      this.datePicker.get(index).triggerInputFocus();
+      this.clickCount++;
+    }, 100);
+  }
+
+  getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
   }
 }
